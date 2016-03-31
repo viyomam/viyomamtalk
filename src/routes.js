ef546b226a42c5150b573691ff00ef3199ddd3d7
Lari Hoppula <lari@sc5.io>
2016-03-31 16:03:48 +0300

    Added /native-login route for react-native authentication

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,16 @@ import Index from './components/Index'
 import Board from './components/Board'
 import Thread from './components/Thread'
 import Profile from './components/Profile'
+import NativeLogin from './components/NativeLogin'
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Index} />
-    <Route path="board/:boardId" component={Board} />
-    <Route path="board/:boardId/:threadId" component={Thread} />
-    <Route path="profile/:uid" component={Profile} />
+  <Route>
+    <Route path="/" component={App}>
+      <IndexRoute component={Index} />
+      <Route path="board/:boardId" component={Board} />
+      <Route path="board/:boardId/:threadId" component={Thread} />
+      <Route path="profile/:uid" component={Profile} />
+    </Route>
+    <Route path="native-login" component={NativeLogin} />
   </Route>
 )
